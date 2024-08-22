8acb594fb22404cc2285b455647c235443b33bef
zuamaya <a01784238@tec.mx>
2024-08-22 14:00:32 -0600

    second clean of code

--- script.py
+++ script.py
@@ -11,6 +11,19 @@ import cv2
 # import matplotlib.pyplot as plt
 from PIL import Image
 
+def get_dimensions_of_any_image(image_path):
+    # Read the image
+    img = cv2.imread(image_path)
+    if img is None:
+        print("Error: could not read image.")
+        return
+    
+    # Get the dimensions of the image
+    height, width, channels = img.shape
+    print("Height:", height)
+    print("Width:", width)
+    print("Channels:", channels)
+
 def get_edges_image(image_path):
     # Read the image
     img = cv2.imread(image_path)
@@ -30,14 +43,14 @@ def get_edges_image(image_path):
 
 def find_first_white_pixel(image_path):
     #Read the image
-    edges_image = Image.open(image_path)
+    img = Image.open(image_path)
 
-    if edges_image is None:
+    if img is None:
         print("Error: could not read image at find_first_white_pixel.")
         return
 
     # Convert image to RGB
-    edges_image = edges_image.convert("RGB")
+    edges_image = img.convert("RGB")
     
     # cv2.imwrite("edges_image_image_color.png", edges_image)
     pixels = edges_image.load()
@@ -94,14 +107,14 @@ def find_first_white_pixel(image_path):
 # This funcion is to see what is the position of the object in the image, it's not used in a real case
 def draw_red_lines(pixel_right_left, pixel_left_right, pixel_top_bottom, pixel_bottom_top, image_path):
     # Read the image with PIL
-    edges_image = Image.open(image_path)
+    img = Image.open(image_path)
 
-    if edges_image is None:
+    if img is None:
         print("Error: could not read image at draw_red_lines.")
         return
 
     # Convert image to RGB
-    edges_image = edges_image.convert("RGB")
+    edges_image = img.convert("RGB")
 
     # Convert image to pixels read format
     pixels = edges_image.load()
@@ -148,15 +161,15 @@ def paste_object_image_in_white_background(pixel_right_left, pixel_left_right, p
 
     return y_position, x_position
 
-def draw_accurate_lines_meansurements(y_position, x_position, centered_img_path):
+def draw_accurate_lines_meansurements(y_position, x_position, image_path):
 
     #Read the image for drawing the measurements
-    img_centered = Image.open(centered_img_path)
-    img_centered_edges = img_centered.convert("RGB")
+    img = Image.open(image_path)
+    img_centered_edges = img.convert("RGB")
     pixels_centered = img_centered_edges.load()
 
     # Get the dimensions of the image
-    height, width, _ = img_centered.shape
+    height, width, _ = img_centered_edges.size
 
     y_position_r = y_position - 80
 
@@ -219,209 +232,4 @@ def draw_accurate_lines_meansurements(y_position, x_position, centered_img_path)
     get_dimensions_of_any_image("centered_image_with_line.png")
 
 
-
-
-
-
-
-def center_image(image_path):
-    # Read the image
-    img = cv2.imread(image_path)
-    if img is None:
-        print("Error: could not read image.")
-        return
     
-    # Convert the image to grayscale
-    gray_test_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-    # Detect edges in the image using the Canny
-    edges = cv2.Canny(gray_test_img, 50, 150)
-
-    # Show the image with edges
-    cv2.imwrite("edges_image.png", edges)
-
-    # Open the image with PIL
-    image_path = "edges_image.png"
-    edges = Image.open(image_path)
-
-    # Convert image to RGB
-    edges = edges.convert("RGB")
-    # cv2.imwrite("edges_image_color.png", edges)
-    pixels = edges.load()
-
-    width, height = edges.size
-    # print("Height of edge image:", height)
-    # print("Width of edge image:", width)
-
-    # Find the first white pixel from right to left
-    line_x = None
-    for x in range(width - 1, -1, -1):
-        for y in range(height):
-            r, g, b = pixels[x, y]
-            if r > 200 and g > 200 and b > 200:  # Considering near white pixel
-                line_x = x
-                break
-        if line_x is not None:
-            break
-    
-    # Find the first white pixel from the left to right
-    line_x_left = None
-    for x in range(width):
-        for y in range(height):
-            r, g, b = pixels[x, y]
-            if r > 200 and g > 200 and b > 200:  # Considering near white pixel
-                line_x_left = x
-                break
-        if line_x_left is not None:
-            break
-    
-    # Find the first white pixel from top to bottom
-    line_y = None
-    for y in range(height):
-        for x in range(width):
-            r, g, b = pixels[x, y]
-            if r > 200 and g > 200 and b > 200:  # Considering near white pixel
-                line_y = y
-                break
-        if line_y is not None:
-            break
-    
-    # Find the first white pixel from bottom to top
-    line_y_top = None
-    for y in range(height - 1, -1, -1):
-        for x in range(width):
-            r, g, b = pixels[x, y]
-            if r > 200 and g > 200 and b > 200:  # Considering near white pixel
-                line_y_top = y
-                break
-        if line_y_top is not None:
-            break
-
-    # Draw a horizontal red line at the found position
-    if line_y and line_y_top is not None:
-        for x in range(width):
-            pixels[x, line_y - 1] = (255, 0, 0)
-            pixels[x, line_y_top + 1] = (255, 0, 0)
-
-    # Draw a vertical red line at the found position
-    if line_x and line_x_left is not None:
-        for y in range(height):
-            pixels[line_x + 1, y] = (255, 0, 0)
-            pixels[line_x_left - 1, y] = (255, 0, 0)
-
-    # Save the modified image
-    edges.save("edges_image_with_line_in_pixel.png")
-
-    # Cut the object from the image
-    cropped_image = img[line_y:line_y_top, line_x_left:line_x]
-    cv2.imwrite("cropped_image.png", cropped_image)
-
-    # Put cropped image in a white background
-    img_cropped = cv2.imread("cropped_image.png")
-    white_background = cv2.imread("./test_images/white_background.jpg")
-
-    if img is None:
-        print("Error: could not read image cropped.")
-        return
-    
-    # Get dimensions of the cropped image and the white background
-    height, width, _ = img_cropped.shape
-    height_white, width_white, _ = white_background.shape
-    print("Height of cropped image:", height)
-    print("Width of cropped image:", width)
-    print("Height of white background:", height_white)
-    print("Width of white background:", width_white)
-
-    # Find the position to paste the cropped image in the white background
-    y_position = (height_white - height) // 2
-    x_position = (width_white - width) // 2
-
-    # Paste the cropped image in the white background
-    white_background[y_position:y_position + height, x_position:x_position + width] = img_cropped
-
-    # Save the image with the object centered in the white background
-    cv2.imwrite("centered_image.png", white_background)
-
-    # Draw the accuotes in the image
-    # Load the image
-    img_complete_edges = Image.open("centered_image.png")
-    edges_complete = img_complete_edges.convert("RGB")
-    pixels_complete = edges_complete.load()
-
-    y_position_r = y_position - 80
-
-    for x in range (x_position, x_position + width + 1):
-        for r in range (5, -1, -1):
-            pixels_complete[x, y_position_r  - r] = (0, 0, 0)
-
-    x_position_r = x_position - 80
-
-    for y in range(y_position, y_position + height + 1):
-        for r in range (5, -1, -1):
-            pixels_complete[x_position_r - r, y] = (0, 0, 0)
-
-    print("y_position_r: ", y_position_r)
-    print("x_position_r: ", x_position_r)
-
-    print("y_position: ", y_position)
-    print("x_position: ", x_position)
-
-    # Draw the remaining line in x axis left 
-    for x in range (x_position - 5, x_position):
-        for r in range (y_position_r + 5, y_position_r - 11, -1):
-            pixels_complete[x, r] = (0, 0, 0)
-
-    # Draw the remaining line in x axis right
-    for x in range (x_position + width + 1, x_position + width + 6):
-        for r in range (y_position_r + 5, y_position_r - 11, -1):
-            pixels_complete[x, r] = (0, 0, 0)
-    
-    # Draw the remaining line in y axis top
-    for y in range (y_position - 5, y_position):
-        for r in range (x_position_r + 5, x_position_r - 11, -1):
-            pixels_complete[r, y] = (0, 0, 0)
-
-    # Draw the remaining line in y axis bottom
-    for y in range (y_position + height + 1, y_position + height + 6):
-        for r in range (x_position_r + 5, x_position_r - 11, -1):
-            pixels_complete[r, y] = (0, 0, 0)
-
-    edges_complete.save("centered_image_with_line.png")
-
-    # Read the image for drawing the measurements
-    img_measuremets = cv2.imread("centered_image_with_line.png")
-    if img is None:
-        print("Error: could not read image.")
-        return
-    
-    height_input = input("Enter the heigh of the object: ")
-    width_input = input("Enter the width of the object: ")
-    font = cv2.FONT_HERSHEY_SIMPLEX
-
-
-    position_with = (x_position + (int(width) // 2) - 40, y_position_r - 40)
-    position_height = (x_position_r - 150, y_position + (int(height) // 2) + 5)
-
-    cv2.putText(img_measuremets, f'{width_input} cm', position_with, font, 1, (0, 0, 0), 1, cv2.LINE_AA)
-    cv2.putText(img_measuremets, f'{height_input} cm', position_height, font, 1, (0, 0, 0), 1, cv2.LINE_AA)
-
-    cv2.imwrite("centered_image_with_line_and_measurements.png", img_measuremets)
-
-    get_dimensions_of_any_image("centered_image_with_line.png")
-    
-
-
-
-def get_dimensions_of_any_image(image_path):
-    # Read the image
-    img = cv2.imread(image_path)
-    if img is None:
-        print("Error: could not read image.")
-        return
-    
-    # Get the dimensions of the image
-    height, width, channels = img.shape
-    print("Height:", height)
-    print("Width:", width)
-    print("Channels:", channels)
-
