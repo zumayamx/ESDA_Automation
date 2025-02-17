e17fed08f39a879d544b481c386fe6519a904c2a
zumayamx <a01784238@tec.mx>
2025-02-16 20:17:26 -0600

    comment sa mask, use rembg instead

--- app/core/image_editor.py
+++ app/core/image_editor.py
@@ -1,78 +1,76 @@
-import cv2
+
 import rembg
 import numpy as np
 from PIL import Image
-
-from segment_anything import SamPredictor, sam_model_registry
-import matplotlib.pyplot as plt
-
-settings = rembg.new_session(
-    alpha_matting=True,
-    alpha_matting_foreground_threshold=220,
-    alpha_matting_background_threshold=30,
-    alpha_matting_erode_size=5
-)
+# from segment_anything import SamPredictor, sam_model_registry
+# import matplotlib.pyplot as plt
 
 class ImageEditor:
     def __init__(self, image_path):
         self.image = Image.open(image_path)
+        self.MAX_BOUNDING_BOX_SIZE = 1000
         if not self.image:
             raise FileNotFoundError
-        
-    def sement_image(self):
-        sam = sam_model_registry["vit_h"](checkpoint="../utils/sam_vit_h_4b8939.pth")
-        predictor = SamPredictor(sam)
-        
-        # Convert PIL image to NumPy array
-        image_array = np.array(self.image)
-
-        # Define point coordinates and labels
-        width, height = self.image.size
-        point_coords = np.array([[width / 2, height / 2]])  # Example coordinates, you should adjust this
-        point_labels = np.array([1])  # Example label, 1 for foreground
-        
-        predictor.set_image(image_array)
-        masks, _, _ = predictor.predict(point_coords=point_coords, point_labels=point_labels)
-
-            # Mostrar la máscara generada
-        plt.figure(figsize=(10, 6))
-        plt.imshow(image_array)
-        plt.imshow(masks[0], cmap="jet", alpha=0.5)  # Superponer la máscara semitransparente
-        plt.title("Máscara generada por Segment Anything")
-        plt.axis("off")
-        plt.show()
     
-    # This is the main function to prepare the image for be edited
-    def add_white_background(self):
-        image_array = np.array(self.image)
-        object_array = rembg.remove(image_array, session=settings)
+    # This function is used to segment the image and get the object mask
+    # it's beta function, we need to figure out if it worth to implement it
+    # def sement_image(self):
+    #     sam = sam_model_registry["vit_h"](checkpoint="../utils/sam_vit_h_4b8939.pth")
+    #     predictor = SamPredictor(sam)
+        
+    #     # Convert PIL image to NumPy array
+    #     image_array = np.array(self.image)
+
+    #     # Define point coordinates and labels
+    #     width, height = self.image.size
+    #     point_coords = np.array([[width / 2, height / 2], [860, height / 2], [2330, height / 2]])
+        
+    #     # Example label, 1 for foreground, 0 for background
+    #     point_labels = np.array([1, 1, 1])
+
+    #     predictor.set_image(image_array)
+    #     masks, _, _ = predictor.predict(point_coords=point_coords, point_labels=point_labels)
+
+    #     # Mostrar la máscara generada
+    #     plt.figure(figsize=(10, 6))
+    #     plt.imshow(image_array)
+    #     plt.imshow(masks[0], cmap="jet", alpha=0.5)
+    #     plt.title("Máscara generada por Segment Anything")
+    #     plt.axis("off")
+    #     plt.show()
+
+    def add_white_background_1(self):
+        # Cargar imagen y eliminar fondo
+        input_data = np.array(self.image)
+        object_array = rembg.remove(input_data)
         object_image = Image.fromarray(object_array)
 
-        # object_image = object_image.convert('RGBA')
-        # pixels = object_image.load()
-        # width, height = object_image.size
+        # Convertir a numpy para trabajar con el canal alfa
+        object_np = np.array(object_image)
 
-        # first_pixel_of_object = None
-        # for x in range(width - 1, -1, -1):
-        #     for y in range(height):
-        #         if pixels[x, y][3] > 0:
-        #             first_pixel_of_object = (x, y)
-        #             break
-        #     if first_pixel_of_object is not None:
-        #         break
+        # Encontrar bounding box usando el canal alfa
+        alpha_channel = object_np[:, :, 3]
+        non_zero_pixels = np.where(alpha_channel > 0)
+
+        # Calcular bounding box
+        if len(non_zero_pixels[0]) > 0:
+            y_min, y_max = non_zero_pixels[0].min(), non_zero_pixels[0].max()
+            x_min, x_max = non_zero_pixels[1].min(), non_zero_pixels[1].max()
+
+            # Recortar la imagen
+            cropped_image = object_image.crop((x_min, y_min, x_max, y_max))
+            cropped_image.save("object_cropped.png")
+
+            # Agregar fondo blanco
+            white_bg = Image.open("../assets/white_bg.png")
+            white_bg.paste(cropped_image, ((white_bg.width - cropped_image.width) // 2, (white_bg.height - cropped_image.height) // 2), cropped_image)
+            white_bg.convert("RGB").save("object_with_white_bg.png")
+
+        else:
+            print("No se detectó ningún objeto en la imagen.")
+ 
         
-        # print('First pixel cordinate of object:', first_pixel_of_object)
-
-        white_background = Image.open("../assets/white_bg.png")
-
-        # object_image = object_image.resize(white_background.size)
-
-        white_background.paste(object_image, (0, 0), object_image)
-        white_background.convert("RGBA")
-        white_background.save("object.png")
-
 if __name__ == '__main__':
-    editor = ImageEditor('../images/IMG_1988.jpg')
-    editor.sement_image()
-    # editor.add_white_background()
-
+    editor = ImageEditor('../images/IMG_2028.jpg')
+    # editor.sement_image()
+    editor.add_white_background_1()
